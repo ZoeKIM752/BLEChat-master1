c66002faa21dba99f05901ff459f1cb53ebc6e2a
ZoeKim <carpediem752752@gmail.com>
2018-09-11 19:10:34 +0900

    Initial commit

---
+++ app/src/main/java/com/hardcopy/blechat/GlobalVar.java
@@ -0,0 +1,69 @@
+package com.hardcopy.blechat;
+
+import android.app.Application;
+
+public class GlobalVar extends Application{
+    private static float ax;
+    private static float ay;
+    private static float az;
+
+    private static float gx;
+    private static float gy;
+    private static float gz;
+
+    private static int mx;
+    private static int my;
+    private static int mz;
+
+    private static float yaw;
+    private static float pitch;
+    private static float roll;
+
+    @Override
+    public void onCreate(){
+
+        super.onCreate();
+    }
+    @Override
+    public void onTerminate(){
+        super.onTerminate();
+    }
+
+    public static void setAx(float ax) { GlobalVar.ax = ax; }
+    public static float getAx() { return GlobalVar.ax; }
+
+    public static void setAy(float ay) { GlobalVar.ay = ay; }
+    public static float getAy() { return GlobalVar.ay; }
+
+    public static void setAz(float az) { GlobalVar.az = az; }
+    public static float getAz() { return GlobalVar.az; }
+
+
+    public static void setGx(float gx) { GlobalVar.gx = gx; }
+    public static float getGx() { return GlobalVar.gx; }
+
+    public static void setGy(float gy) { GlobalVar.gy = gy; }
+    public static float getGy() { return GlobalVar.gy; }
+
+    public static void setGz(float gz) { GlobalVar.gz = gz; }
+    public static float getGz() { return GlobalVar.gz; }
+
+
+    public static void setMx(int mx) { GlobalVar.mx = mx; }
+    public static int getMx() { return GlobalVar.mx; }
+
+    public static void setMy(int my) { GlobalVar.my = my; }
+    public static int getMy() { return GlobalVar.my; }
+
+    public static void setMz(int mz) { GlobalVar.mz = mz; }
+    public static int getMz() { return GlobalVar.mz; }
+
+    public static void setYaw(float yaw) { GlobalVar.yaw = yaw; }
+    public static float getYaw() { return GlobalVar.yaw; }
+
+    public static void setPitch(float pitch) { GlobalVar.pitch = pitch; }
+    public static float getPitch() { return GlobalVar.pitch; }
+
+    public static void setRoll(float roll) { GlobalVar.roll = roll; }
+    public static float getRoll() { return GlobalVar.roll; }
+}
---
+++ app/src/main/java/com/hardcopy/blechat/fragments/ExampleFragment.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2014 Bluetooth Connection Template
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hardcopy.blechat.fragments;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.method.ScrollingMovementMethod;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.hardcopy.blechat.GlobalVar;
+import com.hardcopy.blechat.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@SuppressLint("ValidFragment")
+public class ExampleFragment extends Fragment implements View.OnClickListener {
+
+	private Context mContext = null;
+	private IFragmentListener mFragmentListener = null;
+	private Handler mActivityHandler = null;
+	
+	TextView mTextChat;
+	//EditText mEditChat;
+	Button mBtnSend;
+
+	public ExampleFragment(Context c, IFragmentListener l, Handler h) {
+		mContext = c;
+		mFragmentListener = l;
+		mActivityHandler = h;
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+		
+		mTextChat = (TextView) rootView.findViewById(R.id.text_chat);
+		mTextChat.setMaxLines(1000);
+		mTextChat.setVerticalScrollBarEnabled(true);
+		mTextChat.setMovementMethod(new ScrollingMovementMethod());
+		
+		//mEditChat = (EditText) rootView.findViewById(R.id.edit_chat);
+		//mEditChat.setOnEditorActionListener(mWriteListener);
+		
+		mBtnSend = (Button) rootView.findViewById(R.id.button_send);
+		mBtnSend.setOnClickListener(this);
+		
+		return rootView;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.button_send:
+            //String message = mEditChat.getText().toString();
+            //if(message != null && message.length() > 0)
+            	sendMessage("start");
+			break;
+		}
+	}
+	
+	
+    // The action listener for the EditText widget, to listen for the return key
+    private TextView.OnEditorActionListener mWriteListener =
+        new TextView.OnEditorActionListener() {
+        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+            // If the action is a key-up event on the return key, send the message
+            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                String message = view.getText().toString();
+                if(message != null && message.length() > 0)
+                	sendMessage(message);
+            }
+            return true;
+        }
+    };
+	
+    // Sends user message to remote
+    private void sendMessage(String message) {
+    	if(message == null || message.length() < 1)
+    		return;
+    	// send to remote
+    	if(mFragmentListener != null)
+    		mFragmentListener.OnFragmentCallback(IFragmentListener.CALLBACK_SEND_MESSAGE, 0, 0, message, null,null);
+    	else
+    		return;
+    	// show on text view
+    	if(mTextChat != null) {
+    		mTextChat.append("\nSend: ");
+    		mTextChat.append(message);
+        	int scrollamout = mTextChat.getLayout().getLineTop(mTextChat.getLineCount()) - mTextChat.getHeight();
+        	if (scrollamout > mTextChat.getHeight())
+        		mTextChat.scrollTo(0, scrollamout);
+    	}
+    	//mEditChat.setText("");
+    }
+    
+    private static final int NEW_LINE_INTERVAL = 1000;
+    private long mLastReceivedTime = 0L;
+    
+    // Show messages from remote
+    public void showMessage(String message) {
+    	if(message != null && message.length() > 0) {
+    		long current = System.currentTimeMillis();
+    		
+    		if(current - mLastReceivedTime > NEW_LINE_INTERVAL) {
+    			mTextChat.append("\nRcv: ");
+    		}
+    		parser(message);
+
+    		mTextChat.append("\nax = " + GlobalVar.getAx() + "\tay = " + GlobalVar.getAy() + "\taz = "+ GlobalVar.getAz()
+					+ "\ngx = " + GlobalVar.getGx() + "\tgy = " + GlobalVar.getGy() + "\tgz = "+ GlobalVar.getGz()
+					+ "\nmx = " + GlobalVar.getMx() + "\tmy = " + GlobalVar.getMy() + "\tmz = "+ GlobalVar.getMz()
+					+ "\nYaw = " + GlobalVar.getYaw() + "\tPitch = " + GlobalVar.getPitch() + "\tRoll = "+ GlobalVar.getRoll() + "\n");
+
+        	int scrollamout = mTextChat.getLayout().getLineTop(mTextChat.getLineCount()) - mTextChat.getHeight();
+        	if (scrollamout > mTextChat.getHeight())
+        		mTextChat.scrollTo(0, scrollamout);
+        	
+        	mLastReceivedTime = current;
+    	}
+    }
+
+	public void parser(String message) {
+		message = message.replace("\r\n","");
+    	String[] parseData = message.split(" ");
+		GlobalVar.setAx(Float.parseFloat(parseData[0]));
+		GlobalVar.setAy(Float.parseFloat(parseData[1]));
+		GlobalVar.setAz(Float.parseFloat(parseData[2]));
+		GlobalVar.setGx(Float.parseFloat(parseData[3]));
+		GlobalVar.setGy(Float.parseFloat(parseData[4]));
+		GlobalVar.setGz(Float.parseFloat(parseData[5]));
+		GlobalVar.setMx(Integer.parseInt(parseData[6]));
+		GlobalVar.setMy(Integer.parseInt(parseData[7]));
+		GlobalVar.setMz(Integer.parseInt(parseData[8]));
+		GlobalVar.setYaw(Float.parseFloat(parseData[9]));
+		GlobalVar.setPitch(Float.parseFloat(parseData[10]));
+		GlobalVar.setRoll(Float.parseFloat(parseData[11]));
+	}
+}
